0124b5f22ce8af255a8150a43b0a594e05effbef
JohnTsai960720 <80560777+JohnTsai960720@users.noreply.github.com>
2021-05-29 11:45:58 +0800

    Add files via upload

---
+++ 27.py
@@ -0,0 +1,17 @@
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+
+def buildPyramid(x,y,z):
+    base = 20
+    height = (base//2)
+    for i in range(height):
+        x1=x+i
+        y1=y+i
+        z1=z+i
+        
+        x2=x+base-i
+        z2=z+base-i
+        mc.setBlocks(x1,y1,z1,x2,y,z2,24)
+        
+x,y,z=mc.player.getTilePos()
+buildPyramid(x,y,z)
---
+++ 30.py
@@ -0,0 +1,13 @@
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+x,y,z=mc.player.getPos()
+
+number=1
+
+for i in range(8):
+    
+    for j in range(number):
+        mc.spawnEntity(x,y,z,60)
+        
+    number=number*2
+    
